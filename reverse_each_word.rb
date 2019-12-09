65072829abe0c1a22fd7b148233f104c718ae32e
Paul Leugemors <pleugemors@yahoo.com>
2019-12-09 14:12:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   arr = sentence.split(" ")
   arr2 =[]
-  arr.collect{|word| word.reverse}
-  arr.join(" ")
+  arr.collect{|word| arr2<<word.reverse}
+  arr2.join(" ")
 end
